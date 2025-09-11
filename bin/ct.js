0770222aa2fb2a2af8bb61093cab4bac52236704
lsszz210 <vinedine58@gmail.com>
2025-09-11 22:55:22 +0900

    fix: resolve cli error

--- bin/ct.js
+++ bin/ct.js
@@ -1,25 +1,24 @@
-const pkg = require('../package.json');
 #!/usr/bin/env node
+const pkg = require('../package.json');
 const { program } = require('commander');
 
 program
   .name('ct')
-  .description('\uB300\uD654\uD615 \uBAA8\uB4DC\uB85C Code Tutor CLI\uB97C \uC0AC\uC6A9\uD569\uB2C8\uB2E4')
+  .description('대화형 모드로 Code Tutor CLI를 사용합니다')
   .version(pkg.version || '0.0.0');
 
 // Subcommands
-// tutor command removed
 require('../src/cli/quest')(program);
 require('../src/cli/doctor')(program);
-// Ollama 모델 관리는 ?�거?�었?�니??
 
-// Chat command only (gemini/codex removed)
+// Chat command
 program
   .command('chat')
-  .description('\uB300\uD654\uD615 \uBAA8\uB4DC\uB85C Code Tutor CLI\uB97C \uC0AC\uC6A9\uD569\uB2C8\uB2E4')
+  .description('대화형 모드로 Code Tutor CLI를 사용합니다')
   .action(async () => {
     const startInteractiveMode = require('../src/cli/interactive');
     return startInteractiveMode();
   });
 
 program.parse(process.argv);
+
